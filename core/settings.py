811321079b3f8e6502cf0d40536bdd3ff1f683d2
Asayehu Desalegn <133655930+Asayehu21@users.noreply.github.com>
2025-01-06 15:13:21 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = 'img/'
 MEDIA_ROOT = BASE_DIR/"media"
