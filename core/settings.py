7af1aa06be35287d84de7466e6eff9014dcc32da
Asayehu Desalegn <133655930+Asayehu21@users.noreply.github.com>
2025-01-06 22:29:50 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 
 # APPEND_SLASH = False
 
-ALLOWED_HOSTS = ["bulehoraa-eco.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://bulehoraa-eco.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
