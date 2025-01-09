2a8ddaa1853ca27c63eba4d121d885ce2f889ed9
Asayehu Desalegn <133655930+Asayehu21@users.noreply.github.com>
2025-01-09 22:31:36 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -22,14 +22,12 @@ urlpatterns = [
     path("", include("myshop_app.urls")),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-<<<<<<< HEAD
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> parent of 348b8e9 (1st)
+
