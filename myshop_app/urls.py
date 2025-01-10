6aab8a91d56e8900157ab853cf65950670723342
Asayehu Desalegn <133655930+Asayehu21@users.noreply.github.com>
2025-01-10 10:18:19 +0300

    Update urls.py

--- myshop_app/urls.py
+++ myshop_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import ShippingAddressView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
